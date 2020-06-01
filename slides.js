3cc593515a3befba3f9fe7654bf8a206579e8ac4
yintii <kheart@keleheart.com>
2020-06-01 18:06:35 -0500

    Started to implement the scrolling for the slides, it seems to work fine on mobile but having an issue with the desktop version

---
+++ slides.js
@@ -0,0 +1,33 @@
+const slides = document.querySelector('.slider').children;
+var slide = 0;
+for(i=0;i<slides.length; i++){
+    console.log(`slides[${i}]: `, slides[i])
+}
+
+function rightScroll(){
+    slide += 1;
+    if(slide > slides.length - 1){
+        slide = 0;
+    }
+    changeSlide(slide);
+}
+
+function leftScroll(){
+    slide -= 1;
+    if(slide < 0){
+        slide = slides.length - 1;
+    }
+    changeSlide(slide);
+}
+
+function changeSlide(s){
+    for(i=0;i<slides.length;i++){
+        if(s != i){
+            slides[i].toggleAttribute("hidden");
+            slides[i].removeAttribute("shown");
+        }else{
+            slides[i].toggleAttribute("shown");
+            slides[i].removeAttribute("hidden");
+        }
+    }
+}
